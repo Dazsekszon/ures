87fb61c0a920823cc2292577efa247106ffea820
Dazsekszon <szabo.g.mate@gmail.com>
2025-09-16 12:26:29 +0200

    asd

--- projekt1/src/App.vue
+++ projekt1/src/App.vue
@@ -5,7 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img src="18959fd9-7264-45b4-bf91-ebae400142f9.jpg">
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
--- projekt1/src/main.js
+++ projekt1/src/main.js
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome' 
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faUserAstronaut} from 
 
 createApp(App).mount('#app')
+    .use ()
+    .use ()
